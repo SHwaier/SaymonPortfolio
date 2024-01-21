83bca0b947d3ba3a612d87b2c117d3c0562af947
Unknown159 <59855335+SHwaier@users.noreply.github.com>
2024-01-21 03:04:13 -0500

    fixed braces on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,13 @@ export default function Home() {
       <div className="h-screen">
         <div className="p-12 lg:flex justify-between">
           <div className="flex flex-col justify-center">
-            <p className="text-indigo-500">Hello I am ={"> {"}</p>
+            <p className="text-indigo-500">Hello I am =&gt; &#123;</p>
             <h2 className="md:text-7xl">Saymon Hwaier</h2>
-            <p className="text-indigo-500">{"}"}</p> <p className="text-sm text-indigo-400">//PS: this is my own imaginary syntax :)</p>
-            <div className="py-2">
+            <p className="text-indigo-500">&#125;</p> <p className="text-sm text-indigo-500">//PS: this is my own imaginary syntax :)</p>
+            <div className="py-2 flex items-center">
+              <a href="#">
+                <article data-glow className="p-2 px-3 bg-[var(--background)] border-slate-900 shadow-md border-2">Resume</article>
+              </a>
               <Socials />
             </div>
           </div>
