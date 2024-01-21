d9ab64c0a5cc6aa68c63e80e8ce96050b099bd1c
Unknown159 <59855335+SHwaier@users.noreply.github.com>
2024-01-21 04:07:06 -0500

    showing 3 most recent projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,17 +3,26 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Projects() {
+  function parseDateString(dateString: string): Date {
+    const [month, year] = dateString.split(" ");
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const monthIndex = months.indexOf(month);
+    return new Date(parseInt(year), monthIndex);
+  }
+  
+  let sortedProjects = projectsData.sort((a, b) => parseDateString(b.startDate).getTime() - parseDateString(a.startDate).getTime());
+  sortedProjects = sortedProjects.slice(0,3);
   return (
     <>
       <div className="row">
         <h2 className="text-2xl p-2">Latest Projects</h2>
-        {projectsData.map((item) => (
+        {sortedProjects.map((item) => (
           <div key={item.id} className="p-2">
             <article data-glow className=" md:max-w-2xl">
               <div className="max-w-md bg-[var(--background)] rounded-xl shadow-md overflow-hidden md:max-w-2xl">
                 <div className="md:flex">
                   <div className="md:shrink-0">
-                    <img className="h-48 w-full object-cover md:h-full md:w-48" src={item.img} />
+                    <img className="h-48 w-full object-cover md:h-full md:w-48" src={item.img} alt=""/>
                   </div>
                   <div className="p-8">
                     <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
