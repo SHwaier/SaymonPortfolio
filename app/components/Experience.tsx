ef98e511a1449f6a6df29fe04f1eae1aa6d22f14
Unknown159 <59855335+SHwaier@users.noreply.github.com>
2024-01-22 05:18:57 -0500

    Base Structure down

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,7 +1,23 @@
 
+import experienceData from "../data/experience";
+
 export default function Experience() {
-    return (
-      <><h3>Experience</h3></>
-    );
+  function parseDateString(dateString: string): Date {
+    const [month, year] = dateString.split(" ");
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const monthIndex = months.indexOf(month);
+    return new Date(parseInt(year), monthIndex);
   }
-  
+  let sortedExperience = experienceData.sort((a, b) => parseDateString(b.startDate).getTime() - parseDateString(a.startDate).getTime());
+
+  return (
+    <>
+        
+          {sortedExperience.map((item) => (
+            <div key={item.id}>
+              
+            </div>
+          ))}
+    </>
+  );
+}
